d9b29e97ab42e4f2b5fe0aeb6f235cea923e5b73
Dmitriy Din <dmitriy1d01@gmail.com>
2024-02-18 20:33:02 +0300

    ✅ Initialized

---
+++ app/src/lib/server/repo/index.ts
@@ -0,0 +1,13 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+import {DB} from '$env/static/private'
+// for migrations
+// const migrationClient = postgres(DB, { max: 1 });
+// migrate(drizzle(migrationClient), {})
+
+// for query purposes
+const queryClient = postgres(DB);
+const db = drizzle(queryClient);
+
++
